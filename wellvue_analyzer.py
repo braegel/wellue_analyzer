7c203a27422f19393bb72b750fdec6228132eaf6
Bernd Brägelmann <bernd@braegelmann.net>
2022-03-05 09:45:04 +0100

    added heartrate and dimensions

--- wellvue_analyzer.py
+++ wellvue_analyzer.py
@@ -2,4 +2,5 @@ import sys
 import pandas as pd
 
 df=pd.read_csv(sys.argv[1])
-print ("Median Bloodpressure: %d/%d" % (df['SYS(mmHg)'].median(),df['DIA(mmHg)'].median()))
+print ("Median bloodpressure [mmHG]: %d/%d" % (df['SYS(mmHg)'].median(),df['DIA(mmHg)'].median()))
+print ("Median heartrate [/s]: %d" % (df['PR(bpm)'].median()))
